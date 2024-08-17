3a5657b206d809012a9ea7b5557f2f3b3379bb85
varffs <varffs@users.noreply.github.com>
2024-08-17 20:53:57 +0000

    deploy: fb89e76677e0620661ba5909e98e209827e92502

---
+++ _next/static/81hKTfkZHWD5IQbnKVOLB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/451-128a3d6cfb035079.js","static/css/7dda31b923416813.css","static/chunks/pages/index-1f239adad5b49a30.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
