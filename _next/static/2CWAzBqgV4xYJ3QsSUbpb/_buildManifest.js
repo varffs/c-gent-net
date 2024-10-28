0967f4dbe3c0b25852b12de8841a8c0b7a941d28
crarffs <crarffs@users.noreply.github.com>
2024-10-28 15:56:51 +0000

    deploy: a0f8b9c409c33176a10bc9bcb3876b757e994fb9

---
+++ _next/static/2CWAzBqgV4xYJ3QsSUbpb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/451-e636ec1fa40ec5aa.js","static/css/5ee6016f262f545d.css","static/chunks/pages/index-bfe9b9febb59705d.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
