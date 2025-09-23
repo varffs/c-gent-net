7d3e3bfa16c32f40e8a472f36458c514f3832b26
crarffs <crarffs@users.noreply.github.com>
2025-09-23 19:17:56 +0000

    deploy: f246a0990dcb1f7689b86f8b9b08e39d6c03eea9

---
+++ _next/static/0hQJy86nivGEEB1B4BJdp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/451-f45e803ab2a9e2c0.js","static/css/a3dd29bdbccfc7f9.css","static/chunks/pages/index-bfe9b9febb59705d.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/Ao4A5PMQ1Cy6V7GUSi_Dl/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/451-9f56143725a6fe24.js","static/css/4d5a9c812db6ae99.css","static/chunks/pages/index-bfe9b9febb59705d.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
